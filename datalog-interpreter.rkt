3e5074b69d774f5173ade07485f4df7c6ada27c1
Michael Arntzenius <daekharel@gmail.com>
2017-09-07 21:34:24 +0100

    no more hashes or sets

--- datalog-interpreter.rkt
+++ datalog-interpreter.rkt
@@ -12,43 +12,62 @@
 ;;    X           ;; variables, which are uppercase symbols
 ;;  | x           ;; atoms, which are lowercase symbols
 
-(define (atom? x)
-  (and (symbol? x)
-       (char-lower-case? (string-ref (symbol->string x) 0))))
+(define (pr . args)
+  (apply printf args)
+  (newline))
 
 (define (var? x)
-  (and (symbol? x) (not (atom? x))))
+  (and (symbol? x)
+       (char-upper-case? (string-ref (symbol->string x) 0))))
+
+(define (atom? x) (and (symbol? x) (not (var? x))))
+
+;; "sets" of facts, implemented as lists
+(define empty '())
+(define (singleton x) (list x))
+(define (union xs ys) (remove-duplicates (append xs ys)))
+(define (unions xs) (remove-duplicates (append* xs)))
+(define setof list)
+
+;; substitutions, implemented as association lists
+(define empty-subst '())
+
+(define (lookup subst key
+                [on-failure (lambda () (error "lookup failure"))]
+                [on-success (lambda (x) x)])
+  (match (assoc key subst eq?)
+    [(cons _ r) (on-success r)]
+    [_ (on-failure)]))
+
+(define (update subst key value)
+  ;; assumes key not yet bound in subst
+  (cons (cons key value) subst))
 
 (define (substitute term subst)
   (match term
     ['() '()]
-    [(? var?) (hash-ref subst term (lambda () term))]
+    [(? var?) (lookup subst term (lambda () term))]
     [(? atom?) term]
     [(cons x xs)
      (cons (substitute x subst) (substitute xs subst))]))
 
-(define empty (set))
-(define (singleton x) (set x))
-(define (union xs ys) (set-union xs ys))
-(define (unions xs) (apply set-union (set) xs))
-
 ;; returns #f on unification failure.
 (define (unify subst args grounds)
-  ;; (printf "unifying: ~a ~a ~a\n" subst args grounds)
+  ;; (pr "unifying: ~a ~a ~a" subst args grounds)
   (cond
     [(eq? args grounds) subst]
     [(var? args)
-     (if (hash-has-key? subst args)
-         ;; already substituted for
-         (and (eq? (hash-ref subst args) grounds) subst)
-         ;; bind it
-         (hash-set subst args grounds))]
+     (lookup subst args
+             ;; "failure", variable not yet bound
+             (lambda () (update subst args grounds))
+             ;; "success", variable already substituted for
+             (lambda (v) (and (eq? v grounds) subst)))]
     [(and (cons? args) (cons? grounds))
      (unify (unify subst (car args) (car grounds))
             (cdr args) (cdr grounds))]
     [#t #f]))
 
-(define (solve premises facts [subst (hash)])
+(define (solve premises facts [subst empty-subst])
   (match premises
     ['() (singleton subst)]
     [(cons `(,pred . ,args) premises)
@@ -77,7 +96,8 @@
   (let loop ([facts facts] [n 0])
     (and limit (< limit n) (error "over limit"))
     (define new-facts (step rules facts))
-    ;; (printf "facts: ~v\nnew-facts: ~v\n\n" facts new-facts)
+    ;; (pr "facts: ~v\nnew-facts: ~v\n" facts new-facts)
+    ;; I'm not sure whether equal? is ok here, or whether we need set=?.
     (if (set=? facts new-facts)
         ;; fixed point reached!
         facts
@@ -87,10 +107,10 @@
 
 ;; some examples
 (define some-facts
-  (set
+  (setof
    '(person bob) '(person john)
    '(loves bob john)))
 
 (define some-rules
-  (set
+  (setof
    '((loves X X) (person X))))
