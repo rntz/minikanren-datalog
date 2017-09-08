4a0b47e689c45f874a4cd8172462c9ca328012b0
Michael Arntzenius <daekharel@gmail.com>
2017-09-08 05:21:52 +0100

    IT WORKS! maybe.

--- datalogo2.rkt
+++ datalogo2.rkt
@@ -62,20 +62,25 @@
 
 
 ;; Substitutions as association lists
+
+;; (assoco key val subst)
 (define-rules assoco (key val rest k v)
   [(key val `((,key ,val) ,@rest))]
   [(key val `((,k ,v) ,@rest))
     (=/= key k)
     (assoco key val rest)])
 
+;; (not-assoco key subst)
 (define-rules not-assoco (key rest k v)
   [(key '())]
-  [(key `((,k ,v) ,@rest)) (=/= key k) (not-assoco k rest)])
+  [(key `((,k ,v) ,@rest)) (=/= key k) (not-assoco key rest)])
 
-(define-rules assigno (V G S)
-  [(V G S S) (assoco V G S)]
-  [(V G S `((,V ,G) ,@S)) (not-assoco V S)])
+;; (assigno key value subst subst-out)
+(define-rules assigno (K V S)
+  [(K V S S) (assoco K V S)]
+  [(K V S `((,K ,V) ,@S)) (not-assoco K S)])
 
+;; (substo subst term-in term-out)
 (define-rules substo (S X R X0 X1 R0 R1)
   [(S X X) (atomo X)]
   [(S X X) (varo X) (not-assoco X S)]
@@ -84,6 +89,7 @@
     (substo S X0 R0)
     (substo S X1 R1)])
 
+;; (unifies term ground S S-out)
 (define-rules unifies (M G S S1 S2 M0 M1 G0 G1)
   [(M M S S) (atomo M)]
   [(M G S S1) (varo M) (assigno M G S S1)]
@@ -91,8 +97,21 @@
     (unifies M0 G0 S S1)
     (unifies M1 G1 S1 S2)])
 
+;; (not-unifies term ground S)
+(define-rules not-unifies (M G S S1 M0 M1 G0 G1)
+  [(M G S) (atomo M) (=/= M G)]
+  [((cons M0 M1) G) (atomo G)]
+  [(M G S) (varo M) (assoco M G0 S) (=/= G G0)]
+  [((cons M0 M1) (cons G0 G1) S)
+   (conde
+     [(not-unifies M0 G0 S)]
+     [(unifies M0 G0 S S1) (not-unifies M1 G1 S1)])])
+
+
 
 ;; Querying
+
+;; (query db query subst)
 (define-rules query (DB Q QS S0 S1 fact)
   [(DB '() '())]
   [(DB (cons Q QS) S1)
@@ -127,24 +146,70 @@
 
 
 ;; When is a model/database/set of facts stable? When no rules can fire productively.
-(define (cannot-fire DB rule) 'TODO)
+(define (forall L p)
+  (matche L (x xs)
+    ['()]
+    [(cons x xs) (p x) (forall xs p)]))
 
-(define (nothing-new DB rule) 'TODO)
+;; (query-all S term DB substs)
+;;
+;; `substs`: the list of all substitutions extending `S` which make `term` a
+;; ground element of `DB`.
+(define-rules query-all (S term DB substs fact S-out)
+  [(S term '() '())]
+  [(S term (cons fact DB) (cons S-out substs))
+    (unifies term fact S S-out)
+    ;; now, find the rest of the solutions
+    (query-all S term DB substs)]
+  [(S term (cons fact DB) substs)
+    (not-unifies term fact S)
+    (query-all S term DB substs)])
 
-(define (idempotent DB rule)
-  (conde
-    [(cannot-fire DB rule)]
-    [(nothing-new DB rule)]))
+;; (nothing-new DB conc subst prems)
+(define-rules nothing-new (DB conc S P PS result substs)
+  [(DB conc S '())
+    (substo S conc result)
+    (membero result DB)]
+  [(DB conc S (cons P PS))
+    (query-all S P DB substs)
+    (forall substs (lambda (s) (nothing-new DB conc s PS)))])
 
-(define-rules stable (R RS DB)
-  [(DB '())]
-  [(DB (cons R RS)) (idempotent DB R) (stable DB RS)])
+;; (idempotent DB rule)
+(define-rules idempotent (DB conc prems)
+  [(DB (cons conc prems))
+    (nothing-new DB conc '() prems)])
+
+(define (stable rules DB)
+  (forall rules (curry idempotent DB)))
+
+(define (deduce-all rules init-DB final-DB)
+  (fresh ()
+    (step* rules init-DB final-DB)
+    (stable rules final-DB)))
 
 
 ;; Some examples
-(define p-facts (list '(person bob) '(person john)))
+(define p-facts '((person bob) (person john)))
 (define self-love '((loves 'X 'X) (person 'X)))
 (define p-rules (list self-love))
 
-(define (test)
-  (run 10 (R) (step* p-rules p-facts R)))
+(define p-facts-saturated
+  (append p-facts '((loves bob bob) (loves john john))))
+
+(define (p-test1 [n 10])
+  (run n (R) (step* p-rules p-facts R)))
+
+(define (p-test2 [n 10])
+  (run n (R) (deduce-all p-rules p-facts R)))
+
+
+;;; Graph examples
+(define g-facts '((edge a b) (edge b a)))
+(define g-rules `(((path 'X 'Y) (edge 'X 'Y))
+                  ((path 'X 'Z) (path 'X 'Y) (path 'Y 'Z))))
+
+(define (g-test1 [n 10])
+  (run n (R) (step* g-rules g-facts R)))
+
+(define (g-test2 [n 10])
+  (run n (R) (deduce-all g-rules g-facts R)))
