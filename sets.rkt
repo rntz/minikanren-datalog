cd36b6ba9f9168d3d821a730d0e0eb7c9d9afa0f
Michael Arntzenius <daekharel@gmail.com>
2017-09-14 19:57:59 +0100

    more work on sets.rkt

--- sets.rkt
+++ sets.rkt
@@ -2,9 +2,9 @@
 
 (require "fast-mk/mk.rkt" "macros.rkt" "lists.rkt")
 
-(provide permute)
+(provide permuteo)
 
-(define (permute X Y)
+(define (permuteo X Y)
   (fresh () (length== X Y) (permute-helper X Y)))
 
 (define-rules (permute-helper X Y) (x xs y ys xs^ ys^)
@@ -17,11 +17,24 @@
    (rembero y xs xs^)
    (permute-helper xs^ ys^)])
 
-;; "L without duplicates is S."
-;; this is wrong.
-;;
-;; (define-rules (nubo L S) (x xs S^)
-;;   [('() '())]
-;;   [((cons x xs) S)
-;;    (rembero x S S^)
-;;    (nubo xs S^)])
+;; "L-sans-X is L without any Xes."
+(define-rules (remove-allo X L L-sans-X) (y ys ys^)
+  [(X '() '())]
+  [(X (cons X ys) L-sans-X) (remove-allo X ys L-sans-X)]
+  [(X (cons y ys) (cons y ys^)) (=/= X y) (remove-allo X ys ys^)])
+
+;; "Y is X with only the first instance of each duplicate kept."
+(define-rules (nubo X Y) (x xs xs^ ys)
+  [('() '())]
+  [((cons x xs) (cons x ys))
+   (remove-allo x xs xs^)
+   (nubo xs^ ys)])
+
+;; a list has no duplicates if its nub is itself.
+(define (nodupso X) (nubo X X))
+
+;; subseto and set== work perfectly well (in either direction) if you care about
+;; *all* lists representing a set. but if you care about only duplicate-free
+;; lists, I'm not sure...
+(define (subseto A B) (forallo A (lambda (x) (membero x B))))
+(define (set== A B) (fresh () (subseto A B) (subseto B A)))
